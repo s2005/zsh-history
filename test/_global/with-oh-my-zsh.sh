5257b4d6f9ba571a1da0d5365c23955f66603c44
s2005 <s2005@users.noreply.github.com>
2025-05-11 15:15:04 +0200

    Add missing test scripts for _global directory
    - Added test scripts for all scenarios in the _global directory:
      * with-debian.sh
      * with-alpine.sh
      * with-non-root-user.sh
      * with-oh-my-zsh.sh
    - Made all new test scripts executable
    - This resolves the error in GitHub Actions where it couldn't find the test scripts for the _global directory tests

---
+++ test/_global/with-oh-my-zsh.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# This test file runs for the "with-oh-my-zsh" scenario in _global directory
+
+set -e
+
+# Import test library bundled with the devcontainer CLI
+source dev-container-features-test-lib
+
+# Basic tests
+check "command history directory exists" test -d /commandhistory
+check "zsh history file exists" test -f /commandhistory/.zsh_history
+
+# Oh-My-Zsh tests
+check "oh-my-zsh is installed" test -d ~/.oh-my-zsh
+check "zsh plugins directory has correct permissions" [ "$(stat -c '%U' ~/.oh-my-zsh/custom/plugins)" = "$(whoami)" ]
+
+# Test actual zsh history functionality
+zsh -c 'echo "test command" > /tmp/test_command.txt'
+zsh -c 'cat /tmp/test_command.txt >> $HISTFILE'
+check "zsh history configuration works" grep -q "test command" /commandhistory/.zsh_history
+
+# Report test results
+reportResults
