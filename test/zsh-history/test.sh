12002aa0d536bbeaae3da79eeef874dfc337e1a6
s2005 <s2005@users.noreply.github.com>
2025-07-23 10:53:59 +0000

    Refactor zsh-history test scenarios and scripts for consistency and clarity; remove deprecated tests and add new scenario scripts for Alpine and Debian.

--- test/zsh-history/test.sh
+++ test/zsh-history/test.sh
@@ -7,16 +7,4 @@
 
 set -e
 
-# Import test library bundled with the devcontainer CLI
-source dev-container-features-test-lib
-
-# Source common tests from default.sh
-DEFAULT_TEST_SOURCED=true
-source "$(dirname "$0")/default.sh"
-
-# Run the common tests
-run_basic_checks
-run_zshrc_checks
-
-# Report results
-reportResults
+. default.sh
